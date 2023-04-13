cba5c98dad930a5ee86d67a291f9e1df4170cf7b
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2023-04-12 21:40:32 -0500

    Creando componente Input

---
+++ src/components/Input.js
@@ -0,0 +1,48 @@
+import Styles from '@/styles/Select.module.css'
+
+/**
+ * Componente de selección de opciones.
+ *
+ * @param {object} props - Props del componente.
+ * @param {string} props.value - Valor seleccionado.
+ * @param {string} [props.label] - Texto del label del componente.
+ * @param {function} props.onChange - Función llamada al cambiar la selección.
+ * @param {string} [props.id] - ID del componente.
+ * @param {boolean} [props.required] - Indica si el campo es requerido.
+ * @param {string} [props.placeholder] - Mensaje a mostrar como opción por defecto.
+ * @param {string} [props.pattern] - Expresión regular para validar los datos ingresados por el usuario
+ *
+ * @returns {JSX.Element} Componente de selección de opciones.
+ */
+export default function Input({ value, label, onChange, pattern, ...props }) {
+
+    /**
+     * Función llamada al cambiar la selección.
+     *
+     * @param {object} event - Evento de cambio.
+     */
+    const handleChange = (event) => {
+        onChange(event.target.value)
+    }
+
+    return (
+        <div className={Styles.selectContainer}>
+            <input
+                type={props?.type ?? 'text'}
+                className={Styles.select}
+                value={value}
+                id={props.id}
+                onChange={handleChange}
+                placeholder={props.placeholder}
+                pattern={pattern}
+                required={props.required}
+                {...props} />
+
+            {label && (
+                <label className="select" htmlFor={props.id}>
+                    {label}
+                </label>
+            )}
+        </div>
+    )
+}
