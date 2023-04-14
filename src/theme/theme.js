3379a882b56016e96e38b0f79e3a8cfe2e094e86
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-04-14 12:42:25 -0500

    Agregar: Archivo de configuarción de el tema

---
+++ src/theme/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+  colors: {
+    primary: {
+      1: '#026937',
+      2: '#35944b',
+      3: '#43b649',
+      4: '#8dc63f',
+      5: '#3ebdac'
+    },
+    secondary: {
+      purple: '#70205b',
+      blue: '#137598',
+      red: '#ef434d',
+      yellow: '#f9a12c',
+      violet: '#532d87',
+      green: '#43b649',
+      white: '#ffffff'
+    },
+    others: {
+      text: '#ffffff',
+      background: '#f9f9f9',
+      disabled: '#bfbfbf'
+    }
+  }
+}
