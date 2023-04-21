ac9a9a0994ebb597f6bc0d16721434339ee16b69
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-04-21 14:24:48 -0500

    Actualizar: Gestión de estados, envió y de datos a la API

--- src/pages/create-unit/index.js
+++ src/pages/create-unit/index.js
@@ -1,144 +1,133 @@
 import Card from '@/components/Card'
 import Input from '@/components/Input'
 import Select from '@/components/Select'
-import { useEffect, useState } from 'react'
-import { PlusIcon, CheckIcon, XIcon, ArrowIcon } from '@/components/Icons'
+import { useState } from 'react'
+import { CheckIcon, XIcon, ArrowIcon } from '@/components/Icons'
 import { RoundButton } from '@/components/Buttons'
 import NavBar from '@/components/NavBar'
-import { Router } from 'next/router'
+import { useRouter } from 'next/router'
 
 const BASE_API_URL = 'http://localhost:3000/api'
+const UNIT_TYPES = ['Facultad', 'Escuela', 'Institutos', 'Corporación']
 
 export default function CreateUnit() {
-  const [name, setName] = useState('')
-  const [code, setCode] = useState('')
   const [type, setType] = useState('')
-  const [decane, setDecane] = useState('')
-  const [description, setDescription] = useState('')
-  const [creationUrl, setCreationUrl] = useState('')
-  const [costCenter, setCostCenter] = useState('')
-  const [units, setUnits] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [subunits, setSubunits] = useState([])
+  const router = useRouter()
 
-  useEffect(() => {
-    const fetchUnits = async () => {
-      const response = await fetch(`${BASE_API_URL}/api`)
-      const data = await response.json()
-      setUnits(data)
-      setLoading(false)
-    }
-    fetchUnits()
-  }, [])
+  function addUnit(event, subunit) {
+    event.preventDefault()
+    setSubunits([
+      ...subunits,
+      { name: 'Nueva subunidad', code: subunits.length + 1 }
+    ])
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = Object.fromEntries(new FormData(event.target))
+    formData.subunits = subunits
+    fetch(`${BASE_API_URL}/api`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        router.push('/units')
+      })
+  }
+
+  function handleCancel(event) {
+    event.preventDefault()
+    window.confirm('¿Está seguro que desea cancelar?') && router.push('/units')
+  }
 
   return (
     <>
       <NavBar />
       <main className="container">
         <div style={{ display: 'flex', gap: '1rem' }}>
-          <RoundButton color="yellow" handler={() => Router.back()}>
+          <RoundButton color="yellow" handler={() => router.push('/units')}>
             <ArrowIcon color="white" height="2rem" width="2rem" />
           </RoundButton>
           <h1>Crear Unidad Académica</h1>
         </div>
-        <form
-          style={{
-            width: 'min(55rem, 100%)',
-            display: 'flex',
-            gap: '1.5rem',
-            flexDirection: 'column',
-            margin: 'auto'
-          }}
-        >
+        <form className="container" onSubmit={handleSubmit}>
           <h2>Información general</h2>
-          <Input
-            onChange={setName}
-            value={name}
-            id="name"
-            placeholder="Nombre de la unidad académica"
-            label=""
-            required
-          />
-          <Input
-            onChange={setCode}
-            value={code}
-            id="code"
-            placeholder="Código de la unidad académica"
-            label=""
-            required
-          />
-          <Select
-            onChange={setType}
-            value={type}
-            options={[]}
-            id="type"
-            placeholder="Tipo de unidad académica"
-            required
-          />
-          <Input
-            onChange={setDecane}
-            value={decane}
-            id="dedcane"
-            placeholder="Nombre del decano"
-            label=""
-            required
-          />
-          <Input
-            onChange={setDescription}
-            value={description}
-            id="description"
-            placeholder="Descripción"
-            label=""
-            required
-          />
-          <Input
-            onChange={setCreationUrl}
-            value={creationUrl}
-            id="creationUrl"
-            placeholder="Enlace de acuerdo de creación"
-            label=""
-            required
-          />
-          <Input
-            onChange={setCostCenter}
-            value={costCenter}
-            id="costCenter"
-            placeholder="Código de centro de costos"
-            label=""
-            required
-          />
-        </form>
-        <h2>Subunidades académicas</h2>
-
-        <div className="gridContainer">
-          {units.map((unit) => (
-            <Card
-              key={`${unit.code}`}
-              id={`/units/${unit.code}`}
-              content={unit.name}
+          <fieldset className="subContainer">
+            <Input
+              id="name"
+              placeholder="Nombre de la unidad académica"
+              label="Nombre de la unidad académica"
+              required
             />
-          ))}
-          <Card>
-            <PlusIcon color="white" height="2rem" width="2rem" />
-          </Card>
-        </div>
+            <Input
+              id="code"
+              placeholder="Código de la unidad académica"
+              label="Código de la unidad académica"
+              required
+            />
+            <Select
+              id="type"
+              name="type"
+              onChange={setType}
+              value={type}
+              options={UNIT_TYPES}
+              placeholder="Tipo de unidad académica"
+              label="Tipo de unidad académica"
+              required
+            />
+            <Input
+              id="decane"
+              placeholder="Nombre del decano"
+              label="Nombre del decano"
+              required
+            />
+            <Input
+              id="description"
+              placeholder="Descripción"
+              label="Descripción"
+              required
+            />
+            <Input
+              id="creationUrl"
+              placeholder="Enlace de acuerdo de creación"
+              label="Enlace de acuerdo de creación"
+            />
+            <Input
+              id="costCenter"
+              placeholder="Código de centro de costos"
+              label="Código de centro de costos"
+              required
+            />
+          </fieldset>
+
+          <h2>Subunidades académicas</h2>
+          <fieldset className="subContainer">
+            <div className="gridContainer">
+              {subunits.map((subunit) => (
+                <Card
+                  key={`${subunit.code}`}
+                  id={`/units/${subunit.code}`}
+                  content={subunit.name}
+                />
+              ))}
+              <Card handleAddCard={addUnit} />
+            </div>
+          </fieldset>
+          <div className="fixedContainer">
+            <RoundButton color="green">
+              <CheckIcon width="2rem" height="2rem" color="white" />
+            </RoundButton>
+            <RoundButton color="red" handler={handleCancel}>
+              <XIcon width="2rem" height="2rem" color="white" />
+            </RoundButton>
+          </div>
+        </form>
       </main>
-      <div
-        style={{
-          position: 'absolute',
-          right: '20px',
-          top: '80vh',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '8px'
-        }}
-      >
-        <RoundButton color="green">
-          <CheckIcon width="2rem" height="2rem" color="white" />
-        </RoundButton>
-        <RoundButton color="red">
-          <XIcon width="2rem" height="2rem" color="white" />
-        </RoundButton>
-      </div>
     </>
   )
 }
