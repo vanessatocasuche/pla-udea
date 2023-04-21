de8361ba89c3593397a6494cf10b7823c5856e73
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-04-20 21:38:34 -0500

    Crear: Página para mostrar información por unidad académica

---
+++ src/pages/units/[code].js
@@ -0,0 +1,104 @@
+import { RoundButton } from '@/components/Buttons'
+import Card from '@/components/Card'
+import { ArrowIcon, EditIcon } from '@/components/Icons'
+import NavBar from '@/components/NavBar'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+const ViewUnit = () => {
+  const BASE_API_URL = 'http://localhost:3000/api'
+
+  const router = useRouter()
+  const { code } = router.query
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  function getUnit(code) {
+    fetch(`${BASE_API_URL}/api?code=${code}`, {
+      method: 'GET', // Get, post, put, delete
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[0])
+        setData(data[0])
+        return data
+      })
+  }
+
+  useEffect(() => {
+    if (code) {
+      getUnit(code)
+      setLoading(false)
+    }
+  }, [code])
+
+  return (
+    <>
+      <NavBar />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <main className="container">
+          <div style={{ display: 'flex', gap: '1rem' }}>
+            <RoundButton color="yellow" handler={() => router.back()}>
+              <ArrowIcon color="white" height="2rem" width="2rem" />
+            </RoundButton>
+            <h1>{data.name}</h1>
+          </div>
+          <section
+            className="container"
+            style={{ margin: '0.5rem 0 1rem 3rem' }}
+          >
+            <div>
+              <h3>Descripción de la unidad académica</h3>
+              <p>{data.description}</p>
+            </div>
+            <div>
+              <h3>Tipo de unidad académica</h3>
+              <p>{data.type}</p>
+            </div>
+            <div>
+              <h3>Decano</h3>
+              <p>{data.decane}</p>
+            </div>
+            <div>
+              <h3>Código de la unidad académica</h3>
+              <p>{data.code}</p>
+            </div>
+            <div>
+              <h3>Enlace de acuerdo de creación de la unidad académica</h3>
+              <p>{data.creationUrl}</p>
+            </div>
+            <div>
+              <h3>Código de centro de costos de la unidad académica</h3>
+              <p>{data.costCenter}</p>
+            </div>
+          </section>
+          <h2>Subunidades académicas</h2>
+          <section
+            className="container"
+            style={{ margin: '0.5rem 0 1rem 3rem' }}
+          >
+            {data.subunits ? (
+              <div className="gridContainer">
+                {data.subunits.map((subunit) => (
+                  <Card key={`${subunit}`} content={subunit} />
+                ))}
+              </div>
+            ) : (
+              <p>No hay subunidades académicas registradas</p>
+            )}
+          </section>
+          <RoundButton fixed color="purple">
+            <EditIcon color="white" width="2rem" height="2rem" />
+          </RoundButton>
+        </main>
+      )}
+    </>
+  )
+}
+
+export default ViewUnit
